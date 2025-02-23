54879b1338b39a0d0836121ea48891152c74ef01
Julian Lee <jlee2223@usc.edu>
2025-02-23 12:51:54 -0800

    Update FileProcessor to print each letter on new line

--- FileProcessor.java
+++ FileProcessor.java
@@ -1,12 +1,17 @@
 public class FileProcessor {
     /**
-     * Process a filename using the common validation function
+     * Process a filename using the common validation function and print each letter
      * @param filename Name of the file to process
      * @return Processing status message
      */
     public String processFile(String filename) {
         if (CommonUtils.isValidString(filename)) {
-            return "Processing file: " + filename;
+            System.out.println("Processing file: " + filename);
+            // Print each character of the filename on a new line
+            for (char c : filename.toCharArray()) {
+                System.out.println(c);
+            }
+            return "Processing complete for file: " + filename;
         }
         return "Invalid filename provided";
     }
